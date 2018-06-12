3a507cae713330ffbd5662992a10735af0a0a9bb
Faisal Tameesh <ftameesh@depthsecurity.com>
2018-06-12 14:41:20 -0500

    Parse request file

--- passpr3y.py
+++ passpr3y.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python
+
+import requests
+import pprint
+import json
+
+
+# Parse request file
+requestsFile = open('request.txt', 'r')
+lineList = requestsFile.readlines()
+endPoint = lineList[0].split(' ')[1].strip()
+headerDict = dict(item.split(': ') for item in map(str.strip, lineList[1:-2]))
+dataDict = dict(item.split('=') for item in map(str.strip, lineList[-1].split('&')))
+requestsFile.close()
+
+print len(r.content)
+
+# Load username file
+#r = requests.post("http://" + headerDict["Host"] + '/' + endPoint, \
+#        headers=headerDict, data=dataDict, verify=False)
+
+
