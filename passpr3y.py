d04c2184d0bd491efa424d69d4b2e80c2f9cc040
Faisal Tameesh <ftameesh@depthsecurity.com>
2018-06-22 15:41:24 -0500

    Solve OrderedDict problem
    * Adapt code to Python3.6
    * Update usage warnings

--- passpr3y.py
+++ passpr3y.py
@@ -1,9 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3.6
 
-# This program comes with no promises, warranties, or apologies. 
-# Use at your own risk and responsibility.
+# 1. This program comes with no promises, warranties, or apologies. 
+# 2. Use this program at your own risk and responsibility.
+# 3. When this program is used aggressively, there is a good chance that 
+#       you may lock out a large number of Active Directory 
+#       accounts. Refer to number 2.
+# 4. Default settings of this program are meant to help prevent 
+#       something like that from happening.
 
 import argparse
+import collections
 import requests
 import os
 import sys
@@ -24,36 +30,38 @@ R = '\033[91m'  # red
 W = '\033[0m'   # white
 
 class Passpr3y:
-    def __init__(self, requestFile, usernameFile, passwordFile, duration=7200, ssl=False, shotgun=False):
+    def __init__(self, requestFile, usernameFile, passwordFile, duration=7200, ssl=False, shotgun=False, proxy=None):
         self.requestFile = requestFile
         self.usernameFile = usernameFile
         self.passwordFile = passwordFile
         self.duration = duration
         self.ssl = ssl
         self.shotgun = shotgun
+        self.proxy = { 'http' : proxy, 'https' : proxy}
 
         # Create log directory
         if not os.path.exists("logs"):
             os.makedirs("logs")
 
-        # Parse request file
+        # Parse request file, preserve order of headers
         requestFile = open(self.requestFile, 'r')
         lineList = requestFile.readlines()
+        newlineIndex = lineList.index('\n')
         self.endPoint = lineList[0].split(' ')[1].strip()
-        self.headerDict = dict(item.split(': ') for item in map(str.strip, lineList[1:-2]))
-        self.dataDict = dict(item.split('=') for item in map(str.strip, lineList[-1].split('&')))
+        self.headerDict = collections.OrderedDict(item.split(': ') for item in map(str.strip, lineList[1:newlineIndex]))
+        self.dataDict = collections.OrderedDict(item.split('=') for item in map(str.strip, lineList[newlineIndex+1].split('&')))
         requestFile.close()
         if("USERPR3Y" not in self.dataDict.values() or "PASSPR3Y" not in self.dataDict.values()):
             sys.exit("Error: USERPR3Y or PASSPR3Y not present in POST request parameters.")
 
         # Parse usernames file
         usernameFileHandle = open(self.usernameFile, 'r')
-        self.usernameList = map(str.strip, usernameFileHandle.readlines())
+        self.usernameList = list(map(str.strip, usernameFileHandle.readlines()))
         usernameFileHandle.close()
 
         # Parse passwords file
         passwordFileHandle = open(self.passwordFile, 'r')
-        self.passwordList = map(str.strip, passwordFileHandle.readlines())
+        self.passwordList = list(map(str.strip, passwordFileHandle.readlines()))
         passwordFileHandle.close()
 
         # Figure out time intervals
@@ -61,7 +69,7 @@ class Passpr3y:
         self.slowSleepTime = float(self.shotgunSleepTime)/float(len(self.usernameList))
 
         # Get injection points
-        for key,value in self.dataDict.iteritems():
+        for key,value in self.dataDict.items():
             if value == "USERPR3Y":
                 self.usernameKey = key
             elif value == "PASSPR3Y":
@@ -70,24 +78,26 @@ class Passpr3y:
     def showWarning(self):
         # Ensure spray time is appropriate
         if(not self.shotgun):
-            if raw_input("You will be spraying against " + str(len(self.usernameList)) + " users over the course of " + str(self.shotgunSleepTime) + " seconds.\nThere is a " + str(self.slowSleepTime) + " second wait between each user attempt.\nIs that cool? (y/N) ").lower() != 'y':
+            if input("You will be spraying against " + str(len(self.usernameList)) + " users over the course of " + str(self.shotgunSleepTime) + " seconds.\nThere is a " + str(self.slowSleepTime) + " second wait between each user attempt.\nIs that cool? (y/N) ").lower() != 'y':
                 sys.exit("Change spray time.")
 
         else:
-            if raw_input("You've selected the shotgun method.\nThis will spray ALL users without pausing between each user.\nAfter spraying ALL users, there is a " + str(self.shotgunSleepTime) + " second wait. Opsec is questionable.\nIs that cool? (y/N) ").lower() != 'y':
+            if input("You've selected the shotgun method.\nThis will spray ALL users without pausing between each user.\nAfter spraying ALL users, there is a " + str(self.shotgunSleepTime) + " second wait. Opsec is questionable.\nIs that cool? (y/N) ").lower() != 'y':
                 sys.exit("Don't set shotgun flag.")
 
     def performTest(self):
         randomUser = ''.join(random.choice(string.ascii_lowercase) for _ in range(12))
         randomPass = ''.join(random.choice(string.ascii_lowercase) for _ in range(12))
-        print "%sPerforming test request to benchmark failed attempt...%s" % (Y,W)
+        print("%sPerforming test request to benchmark failed attempt...%s" % (Y,W))
+
         response = self.performRequest(randomUser, randomPass)
+
         if(response.status_code == 400):
-            print "%sTest request returned status code " % (R) + str(response.status_code) + "%s" % (W)
-            if(raw_input("Are you sure you want to continue? (y/N) ") != 'y'):
-                sys.exit("Unsatisfactory response HTTP code.")
+            print("%sTest request returned status code " % (R) + str(response.status_code) + "%s" % (W))
+            if(input("Are you sure you want to continue? (y/N) ") != 'y'):
+                sys.exit("Unsatisfactory HTTP response code.")
         else:
-            print "%sTest request did not return 400, moving on.%s\n" % (G,W)
+            print("%sTest request did not return 400, moving on.%s\n" % (G,W))
 
     def performSpray(self, duration=7200, shotgun=False, ssl=True):
         # Spray
@@ -97,7 +107,7 @@ class Passpr3y:
             tyme = time.strftime("%H:%M:%S", time.localtime())
             responseDict = {}
 
-            print "Password " + str(self.passwordList.index(password) + 1) + " of " + str(len(self.passwordList))
+            print("Password " + str(self.passwordList.index(password) + 1) + " of " + str(len(self.passwordList)))
 
             # Perform spray
             for username in self.usernameList:
@@ -114,7 +124,7 @@ class Passpr3y:
                     time.sleep(self.slowSleepTime)
 
             # Indicate number of unique responses (still basic approach)
-            print "\t\tUnique responses: " + str(len(responseDict))
+            print("\t\tUnique responses: " + str(len(responseDict)))
 
             # Create file
             if not os.path.exists("logs/" + date):
@@ -123,7 +133,7 @@ class Passpr3y:
                 os.makedirs("logs/" + date + '/' + tyme)
 
             # Write to file. Files are named with hashes that distinguish between unique responses.
-            for key,value in responseDict.iteritems():
+            for key,value in responseDict.items():
                 fileOut = open("logs/" + date + '/' + tyme + '/' + key + ".html", 'w')
 
                 # Log request. If there were redirects, log the very first request made.
@@ -138,9 +148,20 @@ class Passpr3y:
                     requestToLog = value.request
 
                 fileOut.write(str(requestToLog.url) + '\n\n')
-                for k2,v2 in requestToLog.headers.iteritems():
+                for k2,v2 in requestToLog.headers.items():
                     fileOut.write(k2 + ": " + v2 + '\n')
                 fileOut.write('\n' + str(requestToLog.body) + '\n')
+
+                if(value.history):
+                    for historyItem in value.history:
+                        fileOut.write('\n' + '-'*80 + '\n')
+                        fileOut.write("RESPONSE")
+                        fileOut.write('\n' + '-'*80 + '\n')
+
+                        fileOut.write(str(historyItem.status_code) + ' ' + historyItem.reason + '\n')
+                        for k2,v2 in historyItem.headers.items():
+                            fileOut.write(k2 + ": " + v2 + '\n')
+                        fileOut.write('\n' + historyItem.text)
 
                 fileOut.write('\n' + '-'*80 + '\n')
                 fileOut.write("RESPONSE")
@@ -148,7 +169,7 @@ class Passpr3y:
 
                 # Log response
                 fileOut.write(str(value.status_code) + ' ' + value.reason + '\n')
-                for k2,v2 in value.headers.iteritems():
+                for k2,v2 in value.headers.items():
                     fileOut.write(k2 + ": " + v2 + '\n')
                 fileOut.write('\n' + value.text)
 
@@ -163,18 +184,44 @@ class Passpr3y:
         self.dataDict[self.passwordKey] = password
         
         # Attempt login
-        print "\tAttempting " + username + ':' + password
+        print("\tAttempting " + username + ':' + password)
         if(self.ssl):
             url = "https://" + self.headerDict["Host"] + self.endPoint
         else:
             url = "http://" + self.headerDict["Host"] + self.endPoint
-        return requests.post(url=url, headers=self.headerDict, data=self.dataDict, verify=False)
 
+        # Convert to string to avoid encoding issues
+        data_str = "&".join("%s=%s" % (k,v) for k,v in self.dataDict.items())
+
+        # Prepare and send request
+        r = requests.Request('POST', url=url, headers=self.headerDict, data=data_str)
+        prepped = r.prepare()
+        s = requests.Session()
+        s.proxies = self.proxy
+        s.verify = False
+
+        return s.send(prepped)
+
+def pretty_print_POST(req):
+    """
+    At this point it is completely built and ready
+    to be fired; it is "prepared".
+
+    However pay attention at the formatting used in
+    this function because it is programmed to be pretty
+    printed and may differ from the actual request.
+    """
+    print('{}\n{}\n{}\n\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
 if __name__ == '__main__':
     # Parse command line arguments
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--request", default="request.txt", help="Name of request file. Default is 'request.txt'.")
+    parser.add_argument("--request", default="request.txt", help="Name of request file in Burp format. Default is 'request.txt'.")
     parser.add_argument("--usernames", default="usernames.txt", help="Name of usernames file. Default is 'usernames.txt'.")
     parser.add_argument("--passwords", default="passwords.txt", help="Name of passwords file. Default is 'passwords.txt'.")
     parser.add_argument("--duration", default="7200", help="Total spray duration in seconds. Default is 7200 seconds.")
@@ -196,16 +243,17 @@ if __name__ == '__main__':
     \tShoutout to the folks at %s@DepthSecurity%s
     """%(R,W,R,W,B,W)
 
-    print "\n" + "-"*73
-    print programheader
-    print "-"*73 + "\n"
+    print("\n" + "-"*73)
+    print(programheader)
+    print("-"*73 + "\n")
 
     pr3y = Passpr3y(requestFile=args.request, \
             usernameFile=args.usernames, \
             passwordFile=args.passwords, \
             duration=args.duration, \
             shotgun=args.shotgun, \
-            ssl=args.ssl)
+            ssl=args.ssl, \
+            proxy=args.proxy)
 
     pr3y.showWarning()
 
